fb0875dec5849b08656eb155d18d23ad4b8597b5
Ary Borenszweig <aborenszweig@manas.com.ar>
2013-09-25 16:41:42 -0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Guisso::Application.config.secret_key_base = 'ba8fbdd88e3e285b11117f25d0b8043b2b3ecd6a242db0331d8a09e4aa20c57d1c1af95b651d8046438f22d3804cf799408d04622d221cbd7264a725c7e9986c'
