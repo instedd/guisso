1b847df64a7984542e4107df0836de89927daf18
Ary Borenszweig <aborenszweig@manas.com.ar>
2013-09-27 17:10:01 -0300

    Store cookie with user email

---
+++ config/initializers/warden.rb
@@ -0,0 +1,10 @@
+Warden::Manager.after_set_user do |user, auth, opts|
+  auth.cookies[:guisso] = {
+    value: user.email,
+    expires: 1.year.from_now,
+    domain: "instedd.org"
+  }
+end
+Warden::Manager.before_logout do |user, auth, opts|
+  auth.cookies.delete(:guisso, domain: "instedd.org")
+end
