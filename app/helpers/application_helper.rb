027f64e9f1da58a2585f309604d643bf11a8e54e
Brian J. Cardiff <bcardiff@manas.com.ar>
2014-01-23 15:36:42 -0300

    render guisso.yml file

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def alto_guisso_yml(application)
+    "enabled: true
+url: #{request.protocol}#{request.host_with_port}
+client_id: #{application.identifier}
+client_secret: #{application.secret}
+"
+  end
 end
