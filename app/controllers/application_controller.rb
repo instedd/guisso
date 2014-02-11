03372d55ea5bcfb6b3cf5303336de191aa97b4b6
Martin Verzilli <mverzilli@manas.com.ar>
2014-02-11 15:08:54 -0300

    Default host for Mailer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,13 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_filter :set_default_host
+
   private
+
+  def set_default_host
+    ActionMailer::Base.default_url_options = {:host => request.host_with_port}
+  end
 
   def after_sign_in_path_for(resource_or_scope)
     if session[:last_oidreq]
