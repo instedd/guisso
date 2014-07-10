f5b32aa86c8af94dbb2ed4f5b6634822f8a99edd
Juan Wajnerman <jwajnerman@manas.com.ar>
2014-07-10 15:42:06 -0300

    Allow sign out with a GET request

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,6 +20,6 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_out_path_for(resource_or_scope)
-    params[:after_sign_out_url].presence || request.referrer
+    params[:after_sign_out_url].presence || request.referrer || root_path
   end
 end
