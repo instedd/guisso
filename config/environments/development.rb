4bb8f46cc54c96312289020fd9a6be9214c90d8c
Juan Wajnerman <jwajnerman@manas.com.ar>
2014-12-03 17:24:14 -0300

    Enable asset digest in development mode to make browsing faster

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,7 @@ Guisso::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  # Generate digests for assets URLs.
+  config.assets.digest = true
 end
