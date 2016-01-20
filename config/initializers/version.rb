7015ba06216772214981a63af3c887ffb30fc3d2
Adrian Romero <aromero@manas.com.ar>
2016-01-20 14:54:03 -0300

    Read VERSION file, cap task to generate version file

--- config/initializers/version.rb
+++ config/initializers/version.rb
@@ -1,7 +1,10 @@
-VersionFilePath = "#{::Rails.root.to_s}/REVISION"
+VersionFilePath = "#{::Rails.root.to_s}/VERSION"
+RevisionFilePath = "#{::Rails.root.to_s}/REVISION"
 
 Guisso::Application.config.send "version_name=", if FileTest.exists?(VersionFilePath) then
   IO.read(VersionFilePath)
+elsif FileTest.exists?(RevisionFilePath)
+  IO.read(RevisionFilePath)
 else
   "development"
 end
