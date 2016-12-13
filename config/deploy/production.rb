b6c47bcbbbc74a4fbb1ac2ecc239dd7469eefa7e
Santiago Palladino <spalladino@gmail.com>
2016-12-13 16:00:29 -0300

    Add missing role to capistrano deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
 set :branch, "master"
 set :deploy_user, 'ec2-user'
 set :force_local_version_matches_deployed, true
-server 'login.instedd.org', user: fetch(:deploy_user), roles: %w{app web}
+server 'login.instedd.org', user: fetch(:deploy_user), roles: %w{app web db}
