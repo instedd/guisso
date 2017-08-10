2cfc36f4a3f49e5d4b3628c4b648bc9a91a02b93
Juan Wajnerman <jwajnerman@manas.com.ar>
2017-08-10 13:11:23 -0300

    Log to stdout when RAILS_LOG_TO_STDOUT is set. Enabled by default in Docker container.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,10 @@ Guisso::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  if ENV["RAILS_LOG_TO_STDOUT"].present?
+    logger           = ActiveSupport::Logger.new(STDOUT)
+    logger.formatter = config.log_formatter
+    config.logger = ActiveSupport::TaggedLogging.new(logger)
+  end
 end
