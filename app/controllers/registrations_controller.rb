ec7c895ab09f60382a4c07920e5c0e70a264df13
Juan Wajnerman <jwajnerman@manas.com.ar>
2017-09-19 20:04:25 -0300

    Added redirect parameter for signup

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,4 +1,11 @@
 class RegistrationsController < Devise::RegistrationsController
+  def new
+    if redirect_url = params[:redirect_url]
+      session[:user_return_to] = redirect_url
+    end
+    super
+  end
+
   def after_inactive_sign_up_path_for(resource)
     new_user_session_path
   end
