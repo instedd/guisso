94c25ddb314c660e6bb51ab3c4d33f908e2a84a8
Juan Wajnerman <jwajnerman@manas.com.ar>
2017-09-19 20:36:41 -0300

    Allow editing user name during sign up and account edit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,16 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_filter :set_default_host
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   layout :layout_by_resource
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :name
+    devise_parameter_sanitizer.for(:account_update) << :name
+  end
 
   private
 
