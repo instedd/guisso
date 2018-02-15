b806884a5d6523547f5978c0094151ddd7e12033
Juan Wajnerman <jwajnerman@manas.com.ar>
2018-02-15 00:07:04 -0300

    Can work with postgres database

--- config/initializers/mysql2.rb
+++ config/initializers/mysql2.rb
@@ -1,5 +1,7 @@
 ActiveSupport.on_load(:active_record) do
-  class ActiveRecord::ConnectionAdapters::Mysql2Adapter
-    NATIVE_DATABASE_TYPES[:primary_key] = "int(11) auto_increment PRIMARY KEY"
+  if defined?(ActiveRecord::ConnectionAdapters::Mysql2Adapter)
+    class ActiveRecord::ConnectionAdapters::Mysql2Adapter
+      NATIVE_DATABASE_TYPES[:primary_key] = "int(11) auto_increment PRIMARY KEY"
+    end
   end
 end
